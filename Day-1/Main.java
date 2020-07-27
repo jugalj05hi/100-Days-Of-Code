03a6323042b9b97649e3afc3441020c9ddef6482
jugalj05hi <jugaljoshi96@gmail.com>
2020-07-27 02:46:48 -0500

    Selection Sort

---
+++ Day-1/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    
+    public static void main(String args[]){
+    
+        int[] arr = {99,33,22,345,32,53};
+
+       SelectionSort sort = new SelectionSort(arr);
+
+       sort.sort();
+        
+    
+    }
+}
---
+++ Day-1/SelectionSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class SelectionSort{
+
+    int[] arr;
+
+    public SelectionSort (int[] arr){
+        this.arr = arr;
+    }
+    // [4,5,3,5,1,9,2]
+    public void sort(){
+
+        for(int i=0; i<arr.length; i++){
+            int index = i;
+            for(int j=i+1; j<arr.length; j++){
+
+                if(arr[j]< arr[index]){
+                   index = j;
+                }
+            }
+            int temp = arr[index];
+            arr[index] = arr[i];
+            arr[i] = temp; 
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
